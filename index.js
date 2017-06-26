36403ea6c036fb2e3e4dc15911e2dac0221d8245
John Lamb <jklamb@iupui.edu>
2017-06-26 16:24:20 -0400

    Add second input and add it to heading

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function handleSubmit(ev) {
     ev.preventDefault()
     const f = ev.target
     const heading = document.querySelector('h1')
-    heading.textContent = f.personName.value
+    heading.textContent = f.personName.value + f.personNickname.value
     
 }
 
