00b24355c76d9b32129b8456a1b61b1a163efaa4
John Lamb <jklamb@iupui.edu>
2017-07-03 19:51:27 -0400

    Build the list in a separate function

--- index.js
+++ index.js
@@ -7,46 +7,50 @@ function renderColor(color) {
     div.style.height = "25px"
 
     return div
-     
+
 }
 
- 
+function renderListItem(label, value) {
+    const item = document.createElement('li')
+    item.textContent = `${label}: ${value}`
+
+    return item
+}
+
+
+function renderList(personData) {
+    const list = document.createElement('ul')
+    
+    // ['name', 'favoriteColor', 'age']
+    Object.keys(personData).map(function(label) {
+        const item = renderListItem(label, personData[label])
+        list.appendChild(item)
+    })
+
+    return list
+}
+
+
 
 function handleSubmit(ev) {
 
     ev.preventDefault()
     const f = ev.target
     const details = document.querySelector('#details')
-    const name = f.personName.value
-    const favoriteColor = f.favoriteColor.value
-    const age = f.age.value
-
-    const colorDiv = `
-         <div style="background-color: ${favoriteColor}; width: 100px; height: 25px;"></div>
-        `
-   
-    // const boldedName = document.createElement('strong')
-    // boldedName.textContent = name
-
-    // details.appendChild(boldedName)
     
-    const nameItem = document.createElement('li')
-    nameItem.textContent = `Name: ${name}`
+      const person = {
+        name: f.personName.value,
+        favoriteColor: f.favoriteColor.value,
+        age: f.age.value,
+    }
 
-    const colorItem = document.createElement('li')
-    colorItem.innerHTML = `Favorite Color: ${renderColor(favoriteColor).outerHTML}`
-    
-    const ageItem = document.createElement('li')
-    ageItem.textContent = `Age: ${age}`
-
-    const list = document.createElement('ul')
-    list.appendChild(nameItem)
-    list.appendChild(colorItem)
-    list.appendChild(ageItem)
-
+    // const colorDiv = `
+    //      <div style="background-color: ${favoriteColor}; width: 100px; height: 25px;"></div>
+    //     `
+    const list = renderList(person)
     details.appendChild(list)
 
-    
+
 }
 
 
